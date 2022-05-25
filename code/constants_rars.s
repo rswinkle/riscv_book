8a66f9e9aac0d2099c7d2659aae0ab6bfe684cd8
Robert Winkler <rob121618@gmail.com>
2022-05-25 16:21:38 -0700

    Converted constants_rars.s

---
+++ code/constants_rars.s
@@ -0,0 +1,15 @@
+
+.eqv sys_print_str 4
+.eqv sys_exit 10
+
+.data
+hello:   .asciz "Hello World!\n"
+
+.text
+main:
+	li   a7, sys_print_str
+	la   a0, hello  # load address of string to print into a0
+	ecall
+
+	li   a7, sys_exit
+	ecall

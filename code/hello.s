19b19d9874024caa3b6f64e86f43dd162c34f7ef
Robert Winkler <rob121618@gmail.com>
2023-01-01 11:24:09 -0700

    Update (c), review ch0

--- code/hello.s
+++ code/hello.s
@@ -3,10 +3,9 @@ hello:   .asciz "Hello World!\n"
 
 .text
 main:
-	li    a7, 4      # print string environment call
-	la    a0, hello  # load address of string to print into a1
+	li   a7, 4      # load immediate, a7 = 4 (4 is print string system call)
+	la   a0, hello  # load address of string to print into a0
 	ecall
 
-	li    a7, 10     # exit ecall
+	li   a7, 10     # exit ecall
 	ecall
-
